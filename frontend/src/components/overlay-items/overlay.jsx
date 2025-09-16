2b96b62fd47d7340497b90380e02c4a6f45ec6df
Ajay <jaykumar002kori@gmail.com>
2025-09-16 17:53:10 +0530

    commit

---
+++ frontend/src/components/overlay-items/overlay.jsx
@@ -0,0 +1,24 @@
+
+// library imports 
+// import React from "react";  // use later if needed.
+
+export function Overlayheader() {
+    return;
+}
+
+export function Overlaylogo() {
+    return;
+}
+
+export function Overlaylist() {
+    return;
+}
+
+export function Overlaybottomstrip() {
+    return;
+}
+
+export function Overlaymiddleheading() {
+    return;
+}
+
