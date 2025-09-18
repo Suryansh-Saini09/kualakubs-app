b15f80058451cbd6cf3889b63d536e8502356a5d
Ajay <jaykumar002kori@gmail.com>
2025-09-18 11:24:08 +0530

    commit

--- frontend/src/config/links.js
+++ frontend/src/config/links.js
@@ -1,15 +1,45 @@
 export const overlayheaderlink = [
-    {pathname: "home", path: "/"},
-    {pathname : "about", path: "/about"},
-    {pathname : "contact", path: "/contact"}
+    { pathname: "home", path: "/" },
+    { pathname: "about", path: "/about" },
+    { pathname: "contact", path: "/contact" }
 ];
 
 export const footerLinks = [
-    { category: "ABOUT", links: [{ path: "/about", pathname: "About us" }, { path: "/about", pathname: "About us" }, { path: "/about", pathname: "About us" }, { path: "/about", pathname: "About us" }] },
-    { category: "ADMISSIONS", links: [{ path: "/about", pathname: "About us" }, { path: "/about", pathname: "About us" }, { path: "/about", pathname: "About us" }, { path: "/about", pathname: "About us" }] },
-    { category: "SCHOOL LIFE", links: [{ path: "/about", pathname: "About us" }, { path: "/about", pathname: "About us" }, { path: "/about", pathname: "About us" }, { path: "/about", pathname: "About us" }] },
-    { category: "QUICK LINKS", links: [{ path: "/about", pathname: "About us" }, { path: "/about", pathname: "About us" }, { path: "/about", pathname: "About us" }, { path: "/about", pathname: "About us" }] },
-    { category: "WEBSITES", links: [{ path: "/about", pathname: "About us" }, { path: "/about", pathname: "About us" }, { path: "/about", pathname: "About us" }, { path: "/about", pathname: "About us" }] }
+    {
+        category: "ABOUT", links:
+            [{ path: "/about", pathname: "About us" },
+            { path: "/about", pathname: "About us" },
+            { path: "/about", pathname: "About us" },
+            { path: "/about", pathname: "About us" }]
+    },
+    {
+        category: "ADMISSIONS", links:
+            [{ path: "/about", pathname: "About us" },
+            { path: "/about", pathname: "About us" },
+            { path: "/about", pathname: "About us" },
+            { path: "/about", pathname: "About us" }]
+    },
+    {
+        category: "SCHOOL LIFE", links:
+            [{ path: "/about", pathname: "About us" },
+            { path: "/about", pathname: "About us" },
+            { path: "/about", pathname: "About us" },
+            { path: "/about", pathname: "About us" }]
+    },
+    {
+        category: "QUICK LINKS", links:
+            [{ path: "/about", pathname: "About us" },
+            { path: "/about", pathname: "About us" },
+            { path: "/about", pathname: "About us" },
+            { path: "/about", pathname: "About us" }]
+    },
+    {
+        category: "WEBSITES", links:
+            [{ path: "/about", pathname: "About us" },
+            { path: "/about", pathname: "About us" },
+            { path: "/about", pathname: "About us" },
+            { path: "/about", pathname: "About us" }]
+    }
     // ... more categories with links
 ];
 
