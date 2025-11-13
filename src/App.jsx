dd608f0dbb2ce15df5c128c47e22b9cce107e711
Ajay <jaykumar002kori@gmail.com>
2025-11-13 15:16:50 +0530

    push

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,27 @@
 
 // Import Router helpers
 import { Routes, Route } from "react-router-dom";
-import { Suspense } from "react";
+// import { Suspense } from "react";
 import routes from "./config/urls.js";
-import Spinner from 'react-bootstrap/Spinner';
+// import Spinner from 'react-bootstrap/Spinner';
 
 function App() {
     return (
         <>
-            <Suspense fallback={
+          {/**  <Suspense fallback={
                 <div className='container-fluid min-vh-100 flex-row d-flex align-items-center justify-content-center'>
                     <h1 className="text-center">Loading</h1>
                     <Spinner animation="grow" variant="success" />
                     <Spinner animation="grow" variant="danger" />
                     <Spinner animation="grow" variant="warning" />
                 </div>
-            }>
+            }> */}
                 <Routes>
                     {routes.map(({ path, element }, index) => (
                         <Route key={index} path={path} element={element} />
                     ))}
                 </Routes>
-            </Suspense>
+            {/**</Suspense> */}
         </>
     );
 }
