529583693990d48faa461b69b6468bb25dd798f1
Ajay <jaykumar002kori@gmail.com>
2025-12-12 13:51:01 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,24 @@
 
 // Import Router helpers
 import { Routes, Route } from "react-router-dom";
-// import { Suspense } from "react";
 import routes from "./config/urls.js";
-// import Spinner from 'react-bootstrap/Spinner';
+import { useEffect } from "react";
 
 function App() {
+    // Prevent browser restoring scroll position
+// ⛔ Prevent browser from restoring previous scroll
+    useEffect(() => {
+        if ("scrollRestoration" in window.history) {
+            window.history.scrollRestoration = "manual";
+        }
+    }, []);
     return (
         <>
-          {/**  <Suspense fallback={
-                <div className='container-fluid min-vh-100 flex-row d-flex align-items-center justify-content-center'>
-                    <h1 className="text-center">Loading</h1>
-                    <Spinner animation="grow" variant="success" />
-                    <Spinner animation="grow" variant="danger" />
-                    <Spinner animation="grow" variant="warning" />
-                </div>
-            }> */}
-                <Routes>
-                    {routes.map(({ path, element }, index) => (
-                        <Route key={index} path={path} element={element} />
-                    ))}
-                </Routes>
-            {/**</Suspense> */}
+            <Routes>
+                {routes.map(({ path, element }, index) => (
+                    <Route key={index} path={path} element={element} />
+                ))}
+            </Routes>
         </>
     );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,17 +3,24 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
+
 //import Navbar from "./components/navbar/Navbar.jsx";
 import OffcanvasNavbar from "./components/navbar/offcanvas.jsx";
 import * as Overlay from "./components/overlay-items/overlay.jsx";
 import * as Links from "./config/links.js";
 import Components from "./components/manage.js";
+import ScrollToTop from "./components/scoller.jsx";
+
+
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
+     
+    <ScrollToTop />
       {/**<Navbar /> */}
       <OffcanvasNavbar />
+      
       <Overlay.Overlayheader headerlinks={Links.overlayheaderlink} />
       <App />
       <Components.Footer footerLinks={Links.footerLinks} />
