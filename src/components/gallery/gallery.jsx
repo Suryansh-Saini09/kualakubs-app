0d48c8edd7ad1ba8b3cf2b710c94c638afab1797
Ajay <jaykumar002kori@gmail.com>
2026-03-24 10:06:47 +0530

    Fix formatting in Gallery component for improved readability

--- src/components/gallery/gallery.jsx
+++ src/components/gallery/gallery.jsx
@@ -23,13 +23,14 @@ export function Gallery({ galleryItems, h, p }) {
                 <div className="container mt-4" style={{ width: "75%" }}>
                     <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
                         {galleryItems.map((item, index) => (
-                            <div className="col" key={index}>
+                            <div className="col" key={index} >
                                 <div className="card shadow position-relative gallery-card-bg rounded-0">
                                     <Link to={item.link}>
                                         <img
                                             src={item.img}
                                             className="img-fluid object-fit-cover h-100 w-100"
                                             alt={item.text}
+                                            style={{minHeight: "226px"}}
                                         />
                                         <span className="gallery-img-text position-absolute">
                                             {item.text}
