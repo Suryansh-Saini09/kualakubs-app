c960e3a8d022e72e69d0fe66b3b2315d1c0b9ee8
Muskan Kaushik <kaushikmuskan2006@gmail.com>
2026-08-25 10:58:45 +0530

    blog 14 updated

--- src/data/blogsData.js
+++ src/data/blogsData.js
@@ -2671,7 +2671,8 @@ export const blogsData = [
   {
     id: "14",
     slug: "best-preschool-and-daycare-in-gurgaon-a-parents-complete-guide",
-    title: "Best Preschool and Daycare in Gurgaon",
+    title: "Best Preschool and Daycare in Gurgaon: A Parent's Complete Guide",
+    metaTitle: "Best Preschool and Daycare in Gurgaon: A Parent's Complete Guide",
     date: "July 25, 2026",
     author: "Admin",
     summary: "If you are searching for the best preschool and day care in Gurgaon or the best preschool near me, then it's essential to find a facility that offers a combination of early education and child development.",
